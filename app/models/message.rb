d6cd651c38d3429cdd111460cbfb25a2ba96a2a6
miiiki <leob76855@gmail.com>
2019-12-05 17:45:36 +0900

    自動更新機能　実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,6 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
+  #validates :created_at, presence: true
   mount_uploader :image, ImageUploader
 end
